97ee9f94702f9bf6e14f31628201ee1ee3727791
Sukh Singh <sukh@orbs.com>
2023-10-16 18:15:41 +0100

    updated root page

--- apps/wallet/src/routes/Root.tsx
+++ apps/wallet/src/routes/Root.tsx
@@ -2,6 +2,7 @@ import {
   Avatar,
   Box,
   Container,
+  Divider,
   HStack,
   Heading,
   Icon,
@@ -9,51 +10,81 @@ import {
   VStack,
 } from '@chakra-ui/react';
 import {
-  Button,
+  Balance,
   Card,
   DataDisplayItem,
   IconButtonWithLabel,
 } from '@telegram-wallet-ui/twa-ui-kit';
-import { BiSolidDownArrowCircle, BiSolidUpArrowCircle } from 'react-icons/bi';
+import {
+  BiPlus,
+  BiSolidDownArrowCircle,
+  BiSolidUpArrowCircle,
+} from 'react-icons/bi';
 import { MdSwapHorizontalCircle } from 'react-icons/md';
-import { FaEthereum } from 'react-icons/fa';
+import { PiPolygon } from 'react-icons/pi';
+import { Link } from 'react-router-dom';
 
 export function Root() {
   return (
     <Container size="sm" pt={4}>
       <VStack spacing={4}>
-        <Heading as="h1" size="lg">
-          Wallet
-        </Heading>
+        <Balance currencySymbol="$" amount={4.63} label="Total balance" />
         <HStack justifyContent="center" alignItems="center" spacing={2}>
-          <IconButtonWithLabel Icon={BiSolidDownArrowCircle} label="Deposit" />
-          <IconButtonWithLabel Icon={BiSolidUpArrowCircle} label="Withdraw" />
-          <IconButtonWithLabel Icon={MdSwapHorizontalCircle} label="Trade" />
+          <Link to="/deposit">
+            <IconButtonWithLabel
+              Icon={BiSolidDownArrowCircle}
+              label="Deposit"
+            />
+          </Link>
+          <Link to="/withdraw">
+            <IconButtonWithLabel Icon={BiSolidUpArrowCircle} label="Withdraw" />
+          </Link>
+          <Link to="/trade">
+            <IconButtonWithLabel Icon={MdSwapHorizontalCircle} label="Trade" />
+          </Link>
         </HStack>
-        <Card>
-          <DataDisplayItem
-            StartIconSlot={
-              <Avatar
-                icon={<Icon as={FaEthereum} fontSize={28} />}
-                bgColor="#A7117A"
-              />
-            }
-            StartTextSlot={
-              <Box>
+        <Link to="/asset/matic" style={{ width: '100%' }}>
+          <Card>
+            <DataDisplayItem
+              StartIconSlot={
+                // TODO: replace with real asset icon - create asset icons in ui kit
+                <Avatar
+                  icon={<Icon as={PiPolygon} fontSize={28} />}
+                  bgColor="#8347E6"
+                />
+              }
+              StartTextSlot={
+                <Box>
+                  <Heading as="h3" variant="bodyTitle">
+                    Polygon
+                  </Heading>
+                  <Text variant="hint">8.86888 MATIC</Text>
+                </Box>
+              }
+              EndTextSlot={
                 <Heading as="h3" variant="bodyTitle">
-                  Ethereum
+                  $4.63
                 </Heading>
-                <Text variant="hint">0.003 ETH</Text>
-              </Box>
-            }
-            EndTextSlot={
-              <Heading as="h3" variant="bodyTitle">
-                $4.63
-              </Heading>
-            }
-          />
-        </Card>
-        <Button>Click me</Button>
+              }
+            />
+          </Card>
+        </Link>
+        <Divider variant="thick" width="80%" />
+        {/* TODO: wrap Link component with custom styling */}
+        <Link to="/assets" style={{ width: '100%' }}>
+          <Card>
+            <DataDisplayItem
+              StartIconSlot={
+                <Avatar icon={<Icon as={BiPlus} fontSize="3xl" />} />
+              }
+              StartTextSlot={
+                <Heading as="h3" variant="bodyTitle">
+                  Add more assets
+                </Heading>
+              }
+            />
+          </Card>
+        </Link>
       </VStack>
     </Container>
   );
