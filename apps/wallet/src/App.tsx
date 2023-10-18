5ecf60d6290f618978c43610e1fb39c3f88ab996
Sukh Singh <sukh@orbs.com>
2023-10-18 09:17:36 +0100

    change buy route to deposit

--- apps/wallet/src/App.tsx
+++ apps/wallet/src/App.tsx
@@ -20,7 +20,7 @@ const router = createBrowserRouter([
     element: <Asset />,
   },
   {
-    path: 'buy',
+    path: 'deposit',
     element: <TransakDeposit walletAddress={account.address} />,
   },
 ]);
