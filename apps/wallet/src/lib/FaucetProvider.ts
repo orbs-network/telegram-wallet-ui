018120ebc6434433b810f65794d1edc30d9f9220
Shahar Yakir <shahar.yakir@gmail.com>
2023-10-19 14:02:54 +0100

    Faucet + permit2 logic (#13)
    * faucet logic
    
    * faucet+approval logic in place

---
+++ apps/wallet/src/lib/FaucetProvider.ts
@@ -0,0 +1,53 @@
+import { Web3Provider } from './Web3Provider';
+import { web3Provider } from '../config';
+import { AuthenticatedTelegramFetcher } from '../utils/fetcher';
+import { getDebug } from './utils/debug';
+import { sleep } from '@defi.org/web3-candies';
+const debug = getDebug('FaucetProvider');
+
+export class FaucetProvider {
+  private POLL_INTERVAL = 3000;
+  private currentErc20TokenPolled: string | null = null;
+
+  constructor(private faucetUrl: string, web3Provider: Web3Provider) {
+    debug('done');
+  }
+
+  private async requestFromFaucet(erc20Token: string) {
+    debug('Requesting from faucet');
+    await AuthenticatedTelegramFetcher.post(`${this.faucetUrl}/topUp`, {
+      toAddress: web3Provider.account.address,
+      erc20Token,
+    });
+  }
+
+  async requestIfNeeded(erc20Token: string) {
+    this.currentErc20TokenPolled = erc20Token;
+
+    debug(
+      'Checking native balance for address %s',
+      web3Provider.account.address
+    );
+    if (!(await web3Provider.balance()).isZero()) {
+      debug('native balance is non-zero, skipping');
+      return false;
+    }
+
+    debug(
+      'Checking erc20 balance for address %s',
+      web3Provider.account.address
+    );
+
+    while ((await web3Provider.balanceOf(erc20Token)).isZero()) {
+      if (this.currentErc20TokenPolled !== erc20Token) {
+        debug('erc20 token changed, skipping');
+        return false;
+      }
+      debug('erc20 balance is zero, sleeping for %d ms', this.POLL_INTERVAL);
+      await sleep(this.POLL_INTERVAL);
+    }
+
+    await this.requestFromFaucet(erc20Token);
+    return true;
+  }
+}
--- apps/wallet/src/utils/fetcher.ts
+++ apps/wallet/src/utils/fetcher.ts
@@ -6,13 +6,30 @@ export class Fetcher {
 
   static async post<T = unknown>(url: string, body: any): Promise<T> {
     const res = await fetch(url, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(body),
     });
 
     return res.json() as Promise<T>;
+  }
+}
+
+declare global {
+  interface Window {
+    Telegram: any;
+  }
+}
+
+export class AuthenticatedTelegramFetcher extends Fetcher {
+  static async post<T = unknown>(url: string, body: any): Promise<T> {
+    return super.post(url, {
+      ...body,
+      // queryData: window.Telegram?.WebApp.initData,
+      queryData:
+        'query_id=AAF9SbUDAAAAAH1JtQNq4_8C&user=%7B%22id%22%3A62212477%2C%22first_name%22%3A%22shahar%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22mrbonezy%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1697629175&hash=9590293251587e9d290ca1505c50bbba058e1e2ab55d1874d6f19c8210acffb6',
+    });
   }
 }
