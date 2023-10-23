0674ff6b25954521789198318562c176bf1203dd
Shahar Yakir <shahar.yakir@gmail.com>
2023-10-23 15:38:23 +0100

    debounce

--- apps/wallet/src/lib/SwapProvider.ts
+++ apps/wallet/src/lib/SwapProvider.ts
@@ -1,8 +1,12 @@
 import { BNComparable, QuoteResponse } from '../types';
 import { LiquihubProvider } from './LiquihubProvider';
 import { CoinsProvider } from './CoinsProvider';
+import { getDebug } from './utils/debug';
+import BN from 'bignumber.js';
 
-type Quote = {
+const debug = getDebug('SwapProvider');
+
+type QuoteRequest = {
   inToken: string;
   outToken: string;
   inAmount: BNComparable;
@@ -14,17 +18,35 @@ export class SwapProvider {
     private liquidityHubProvider: LiquihubProvider
   ) {}
 
-  async quote(_quote: Quote) {
+  async quote(quoteRequest: QuoteRequest) {
     // TODO cache
     const fetchCoins = await this.coinsProvider.fetchCoins();
 
-    const price = await this.coinsProvider.getMinAmountOut(
-      fetchCoins.find((c) => c.address === _quote.inToken)!,
-      fetchCoins.find((c) => c.address === _quote.outToken)!,
-      _quote.inAmount
+    const minOutAmount = await this.coinsProvider.getMinAmountOut(
+      fetchCoins.find((c) => c.address === quoteRequest.inToken)!,
+      fetchCoins.find((c) => c.address === quoteRequest.outToken)!,
+      quoteRequest.inAmount
     );
 
-    return this.liquidityHubProvider.quote({ ..._quote, outAmount: price });
+    const quoteResp = await this.liquidityHubProvider.quote({
+      ...quoteRequest,
+      outAmount: minOutAmount,
+    });
+
+    const isAboveMin = BN(quoteResp.outAmount).gte(minOutAmount);
+
+    debug(
+      `Got quote from LiquidityHub: ${
+        quoteResp.outAmount
+      }, min amount ${minOutAmount}, delta ${BN(quoteResp.outAmount).dividedBy(
+        minOutAmount
+      )} isAboveMin: ${isAboveMin} `
+    );
+
+    return {
+      quote: quoteResp,
+      isAboveMin,
+    };
   }
 
   async swap(quote: QuoteResponse) {
