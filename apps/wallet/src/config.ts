b9c7077234097293d64e4007a75391a613f29cfe
Shahar Yakir <shahar.yakir@gmail.com>
2023-10-24 09:43:31 +0100

    in prog

--- apps/wallet/src/config.ts
+++ apps/wallet/src/config.ts
@@ -5,9 +5,9 @@ import { FaucetProvider } from './lib/FaucetProvider';
 
 // export const w3 = new web3(networks.poly.publicRpcUrl);
 export const w3 = new web3(
-  `https://polygon-mumbai.g.alchemy.com/v2/${
-    import.meta.env.VITE_ALCHEMY_API_KEY
-  }`
+  `https://polygon-${
+    import.meta.env.VITE_IS_MUMBAI === '1' ? 'mumbai' : 'mainnet'
+  }.g.alchemy.com/v2/${import.meta.env.VITE_ALCHEMY_API_KEY}`
 );
 export const TRANSAK_STAGING_API_KEY = import.meta.env
   .VITE_TRANSAK_STAGING_API_KEY;
@@ -18,11 +18,11 @@ export const account = accountHolder.account;
 
 export const web3Provider = new Web3Provider(w3, account);
 
+export const erc20sDataProvider = new ERC20sDataProvider();
+
 export const liqHubProvider = new LiquihubProvider(web3Provider);
 
 export type CryptoAsset = 'MATIC' | 'ETH' | 'USDC';
-
-export const erc20sDataProvider = new ERC20sDataProvider();
 
 export const faucetProvider = new FaucetProvider(
   import.meta.env.VITE_FAUCET_BACKEND_URL,
