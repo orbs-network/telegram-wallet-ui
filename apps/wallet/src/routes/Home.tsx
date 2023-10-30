94a1419f43acf4a151becebb89d934c8fb36549e
Sukh Singh <sukh@orbs.com>
2023-10-30 11:55:45 +0000

    added mock txs for show

--- apps/wallet/src/routes/Home.tsx
+++ apps/wallet/src/routes/Home.tsx
@@ -2,17 +2,18 @@ import { Container, HStack, VStack } from '@chakra-ui/react';
 import { Balance, IconButtonWithLabel } from '@telegram-wallet-ui/twa-ui-kit';
 import { BiSolidDownArrowCircle, BiSolidUpArrowCircle } from 'react-icons/bi';
 import { Link } from 'react-router-dom';
-import { Page, TokenBalances } from '../components';
+import { Page, TokenBalances, Transactions } from '../components';
 import { useFormatNumber, usePortfolioUsdValue } from '../hooks';
 import { faucetProvider, isMumbai, permit2Provider } from '../config';
 import { MdSwapVerticalCircle } from 'react-icons/md';
+import { mockTransactions } from '../mocks/transactions';
 
 // Temp - USDC
 const TODO_TEMP_ERC20_REPLACE = isMumbai
   ? '0x0FA8781a83E46826621b3BC094Ea2A0212e71B23'
   : '0x2791bca1f2de4661ed88a30c99a7a9449aa84174';
 
-// TODO(sukh) - 
+// TODO(sukh) -
 // 1. set this in the deposit flow
 // 2. call this for the inToken in the trade confirmation screen
 // permit2Provider.addErc20(TODO_TEMP_ERC20_REPLACE);
@@ -22,7 +23,6 @@ permit2Provider.pollPermit2Approvals();
 
 // Polls until the current selected erc20 was transferred to this account
 faucetProvider.requestIfNeeded();
-
 
 const TotalUSDAmount = () => {
   const usdValue = usePortfolioUsdValue();
@@ -64,6 +64,7 @@ export function Home() {
             </Link>
           </HStack>
           <TokenBalances />
+          <Transactions transactions={mockTransactions} />
         </VStack>
       </Container>
     </Page>
