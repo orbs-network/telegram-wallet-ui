fc0f9edc15e2f982aa0a4a9a384a28e7d5b2ff19
Sukh Singh <sukh@orbs.com>
2023-11-01 17:22:37 +0000

    updated env vat name

--- apps/wallet/src/config.ts
+++ apps/wallet/src/config.ts
@@ -16,8 +16,7 @@ export const w3 = new web3(
     import.meta.env.VITE_ALCHEMY_API_KEY
   }`
 );
-export const TRANSAK_STAGING_API_KEY = import.meta.env
-  .VITE_TRANSAK_STAGING_API_KEY;
+export const TRANSAK_API_KEY = import.meta.env.VITE_TRANSAK_API_KEY;
 
 export const accountProvider = new AccountProvider(w3);
 
