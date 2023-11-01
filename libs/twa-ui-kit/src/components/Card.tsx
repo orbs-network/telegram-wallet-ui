f035203d3fe719c7a3b4a9cf141f519cc6847459
Sukh Singh <sukh@orbs.com>
2023-11-01 11:23:56 +0000

    forcing card bg color

--- libs/twa-ui-kit/src/components/Card.tsx
+++ libs/twa-ui-kit/src/components/Card.tsx
@@ -1,11 +1,13 @@
 import { CardBody, Card as ChakraCard } from '@chakra-ui/react';
 import { css } from '@emotion/react';
+import { colors } from '../theme';
 
 const styles = css`
   border-radius: 1rem;
   min-height: 72px;
   width: 100%;
   box-shadow: unset;
+  background-color: ${colors.bg_color};
 `;
 
 type CardProps = {
