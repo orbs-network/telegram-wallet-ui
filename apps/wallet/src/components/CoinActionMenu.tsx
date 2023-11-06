5c4d26431a1f33047e88218d124c686733e22320
Sukh Singh <sukh@orbs.com>
2023-11-06 10:58:54 +0000

    removed buy option in asset action menu

--- apps/wallet/src/components/CoinActionMenu.tsx
+++ apps/wallet/src/components/CoinActionMenu.tsx
@@ -1,7 +1,7 @@
 import { HStack } from '@chakra-ui/react';
 import { IconButtonWithLabel } from '@telegram-wallet-ui/twa-ui-kit';
 import { Link, generatePath } from 'react-router-dom';
-import { BuyIcon, DepositIcon, TradeIcon, WithdrawIcon } from './icons';
+import { DepositIcon, TradeIcon, WithdrawIcon } from './icons';
 import Twa from '@twa-dev/sdk';
 import { ROUTES } from '../router/routes';
 
@@ -18,13 +18,7 @@ type CoinActionMenuProps = {
 };
 
 export function CoinActionMenu({ tokenSymbol }: CoinActionMenuProps) {
-  const depositPath = generatePath(ROUTES.depositCrypto, {
-    assetId: tokenSymbol,
-  });
-
-  const buyPath = generatePath(ROUTES.depositBuy, {
-    assetId: tokenSymbol,
-  });
+  const depositPath = ROUTES.deposit;
 
   const tradePath = `${ROUTES.trade}?inToken=${tokenSymbol}`;
 
@@ -36,9 +30,6 @@ export function CoinActionMenu({ tokenSymbol }: CoinActionMenuProps) {
     <HStack justifyContent="center" alignItems="center" spacing={8} pt={2}>
       <Link to={depositPath} onClick={handleClick}>
         <IconButtonWithLabel IconSlot={<DepositIcon />} label="Deposit" />
-      </Link>
-      <Link to={buyPath} onClick={handleClick}>
-        <IconButtonWithLabel IconSlot={<BuyIcon />} label="Buy" />
       </Link>
       <Link to={tradePath} onClick={handleClick}>
         <IconButtonWithLabel IconSlot={<TradeIcon />} label="Trade" />
