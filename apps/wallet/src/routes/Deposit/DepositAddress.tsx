acfa8e7bf579fc1e4996fb29feebbb69c0f6e08e
Luke Rogerson <contact@lukerogerson.me>
2023-11-07 15:35:47 +0000

    Fix duplicate toast being displayed upon copy address

--- apps/wallet/src/routes/Deposit/DepositAddress.tsx
+++ apps/wallet/src/routes/Deposit/DepositAddress.tsx
@@ -17,6 +17,7 @@ import { NetworkSelector, Page, WalletSpinner } from '../../components';
 import { useParams } from 'react-router-dom';
 import { URLParams } from '../../types';
 import { BiSolidCopy } from 'react-icons/bi';
+
 const QR_SIZE = 190;
 const isShareSupported = navigator.share !== undefined;
 
@@ -161,6 +162,8 @@ const ShareButton = ({ address }: { address: string }) => {
   );
 };
 
+const copyToastId = 'copy-toast';
+
 const CopyButton = ({ address }: { address: string }) => {
   const toast = useToast();
 
@@ -168,12 +171,15 @@ const CopyButton = ({ address }: { address: string }) => {
     navigator.clipboard
       .writeText(address)
       .then(() => {
-        toast({
-          status: 'success',
-          title: 'Copied to clipboard',
-          isClosable: true,
-          duration: 40_000,
-        });
+        if (!toast.isActive(copyToastId)) {
+          toast({
+            status: 'success',
+            title: 'Copied to clipboard',
+            isClosable: true,
+            duration: 3_000,
+            id: copyToastId,
+          });
+        }
       })
       .catch((err) => {
         console.error('Failed to copy text: ', err);
@@ -198,8 +204,8 @@ const Warning = () => {
   return (
     <Text css={styles.warning}>
       Send only <strong>{token?.symbolDisplay || ''} ERC-20</strong> to this
-      Polygon address using a native Polygon wallet. <br /> Sending other coins may
-      result in permanent loss.
+      Polygon address using a native Polygon wallet. <br /> Sending other coins
+      may result in permanent loss.
     </Text>
   );
 };
