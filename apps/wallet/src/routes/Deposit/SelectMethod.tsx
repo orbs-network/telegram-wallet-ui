318278d94d34f2ab36c29b40ed0e33842840c9ba
Shahar Yakir <shahar.yakir@gmail.com>
2023-11-07 16:20:26 +0000

    added onboarding card

--- apps/wallet/src/routes/Deposit/SelectMethod.tsx
+++ apps/wallet/src/routes/Deposit/SelectMethod.tsx
@@ -9,8 +9,6 @@ import {
 } from '@chakra-ui/react';
 import { css } from '@emotion/react';
 import {
-  Card,
-  DataDisplayItem,
   colors,
   List,
   ListItem,
@@ -26,7 +24,72 @@ import { ROUTES } from '../../router/routes';
 const styles = {
   list: css`
     gap: 10px;
-  `
+  `,
+};
+
+export function DepositMethods() {
+  return (
+    <List mode="display" css={styles.list}>
+      <Link to={ROUTES.depositBuy}>
+        <ListItem
+          StartTextSlot={
+            <Box>
+              <Heading as="h3" variant="bodyTitle">
+                Bank Card
+              </Heading>
+              <Text variant="hint">
+                Purchase USD stablecoin with a debit/credit card
+              </Text>
+            </Box>
+          }
+          StartIconSlot={
+            <Avatar
+              width="40px"
+              height="40px"
+              icon={<Icon as={AiOutlineCreditCard} />}
+              bgColor={colors.button_color}
+            />
+          }
+          EndIconSlot={
+            <Icon
+              as={BiChevronRight}
+              color={colors.hint_color}
+              fontSize="4xl"
+            />
+          }
+        />
+      </Link>
+      <Link to={generatePath(ROUTES.depositSelectCoin, { method: 'crypto' })}>
+        <ListItem
+          StartIconSlot={
+            <Avatar
+              width="40px"
+              height="40px"
+              icon={<Icon as={RiApps2Line} />}
+              bgColor={colors.button_color}
+            />
+          }
+          StartTextSlot={
+            <Box>
+              <Heading as="h3" variant="bodyTitle">
+                External Wallet
+              </Heading>
+              <Text variant="hint">
+                Deposit crypto from any external wallet on any chain
+              </Text>
+            </Box>
+          }
+          EndIconSlot={
+            <Icon
+              color={colors.hint_color}
+              as={BiChevronRight}
+              fontSize="4xl"
+            />
+          }
+        />
+      </Link>
+    </List>
+  );
 }
 
 export function SelectMethod() {
@@ -37,68 +100,7 @@ export function SelectMethod() {
           <Heading as="h1" size="md" textAlign="center">
             How would you like to deposit crypto?
           </Heading>
-          <List mode="display" css={styles.list}>
-            <Link to={ROUTES.depositBuy}>
-              <ListItem
-                StartTextSlot={
-                  <Box>
-                    <Heading as="h3" variant="bodyTitle">
-                      Bank Card
-                    </Heading>
-                    <Text variant="hint">
-                      Purchase USD stablecoin with a debit/credit card
-                    </Text>
-                  </Box>
-                }
-                StartIconSlot={
-                  <Avatar
-                    width="40px"
-                    height="40px"
-                    icon={<Icon as={AiOutlineCreditCard} />}
-                    bgColor={colors.button_color}
-                  />
-                }
-                EndIconSlot={
-                  <Icon
-                    as={BiChevronRight}
-                    color={colors.hint_color}
-                    fontSize="4xl"
-                  />
-                }
-              />
-            </Link>
-            <Link
-              to={generatePath(ROUTES.depositSelectCoin, { method: 'crypto' })}
-            >
-              <ListItem
-                StartIconSlot={
-                  <Avatar
-                    width="40px"
-                    height="40px"
-                    icon={<Icon as={RiApps2Line} />}
-                    bgColor={colors.button_color}
-                  />
-                }
-                StartTextSlot={
-                  <Box>
-                    <Heading as="h3" variant="bodyTitle">
-                      External Wallet
-                    </Heading>
-                    <Text variant="hint">
-                      Deposit crypto from any external wallet on any chain
-                    </Text>
-                  </Box>
-                }
-                EndIconSlot={
-                  <Icon
-                    color={colors.hint_color}
-                    as={BiChevronRight}
-                    fontSize="4xl"
-                  />
-                }
-              />
-            </Link>
-          </List>
+          <DepositMethods />
         </VStack>
       </Container>
     </Page>
