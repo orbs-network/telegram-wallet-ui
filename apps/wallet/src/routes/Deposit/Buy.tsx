d11a163714974a55acf9af990594e1e3c4560718
Luke Rogerson <contact@lukerogerson.me>
2023-11-07 18:25:37 +0000

    Trigger faucet and permit2 for USDT from on ramp screen (#86)

--- apps/wallet/src/routes/Deposit/Buy.tsx
+++ apps/wallet/src/routes/Deposit/Buy.tsx
@@ -4,19 +4,27 @@ import { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ErrorPage } from '../../ErrorPage';
 import { Page } from '../../components';
-import { TRANSAK_API_KEY, TRANSAK_URL, account } from '../../config';
+import {
+  TRANSAK_API_KEY,
+  TRANSAK_URL,
+  account,
+  faucetProvider,
+  permit2Provider,
+} from '../../config';
+import { useGetTokenFromList } from '../../hooks';
 import { getDebug } from '../../lib/utils/debug';
 import { Transak } from './transak/constants';
 
 const debug = getDebug('Buy');
 
 const walletAddress = account?.address;
+const SYMBOL = 'USDT';
 
 const constructSrcUrl = (walletAddress: string) => {
   const params = new URLSearchParams({
     network: 'polygon',
     walletAddress,
-    cryptoCurrencyCode: 'USDT',
+    cryptoCurrencyCode: SYMBOL,
     disableWalletAddressForm: 'true',
     hideMenu: 'true',
     themeColor: colors.button_color,
@@ -29,6 +37,15 @@ export const Buy = () => {
   const [showDoneButton, setShowDoneButton] = useState(false);
   const iframeRef = useRef<HTMLIFrameElement>(null);
   const navigate = useNavigate();
+  const usdtToken = useGetTokenFromList(SYMBOL);
+
+  useEffect(() => {
+    if (usdtToken) {
+      debug('Triggering faucet and permit2');
+      faucetProvider.setProofErc20(usdtToken.address);
+      permit2Provider.addErc20(usdtToken.address);
+    }
+  }, [usdtToken]);
 
   useEffect(() => {
     const handleMessage = (message: {
