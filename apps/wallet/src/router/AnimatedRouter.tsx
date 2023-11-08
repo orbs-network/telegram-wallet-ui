9087230fff4efaf4cdd57d91a1888879dd49accb
Sukh Singh <sukh@orbs.com>
2023-11-08 01:38:22 +0000

    improved route match

--- apps/wallet/src/router/AnimatedRouter.tsx
+++ apps/wallet/src/router/AnimatedRouter.tsx
@@ -4,8 +4,8 @@ import {
   Route,
   RouteObject,
   Routes,
+  matchRoutes,
   useLocation,
-  useMatch,
   useNavigate,
   useNavigationType,
 } from 'react-router-dom';
@@ -15,6 +15,18 @@ import { ROUTES } from './routes';
 import { useCurrentPath } from '../hooks';
 import { BackButton, setTwaBg } from '@telegram-wallet-ui/twa-ui-kit';
 
+const routesToMatch = [
+  ROUTES.withdrawSuccess,
+  ROUTES.tradeSuccess,
+  ROUTES.trade,
+  ROUTES.tradeInTokenSelect,
+  ROUTES.tradeOutTokenSelect,
+].map((route) => {
+  return {
+    path: route,
+  };
+});
+
 type ContextType = {
   navigationType: NavigationType;
 };
@@ -23,36 +35,17 @@ const Context = React.createContext({ navigationType: 'PUSH' } as ContextType);
 const AnimatedRouter = ({ routes }: { routes: RouteObject[] }) => {
   const location = useLocation();
   const navigationType = useNavigationType();
-
-  // TODO: must be a better way to match multiple routes
-  const isWithdrawSuccess = useMatch(ROUTES.withdrawSuccess);
-  const isTradeSuccess = useMatch(ROUTES.tradeSuccess);
-  const isTrade = useMatch(ROUTES.trade);
-  const isTradeIn = useMatch(ROUTES.tradeInTokenSelect);
-  const isTradeOut = useMatch(ROUTES.tradeOutTokenSelect);
+  const match = matchRoutes(routesToMatch, location);
 
   const homePage = location.pathname === ROUTES.root;
 
   useEffect(() => {
-    if (
-      isWithdrawSuccess ||
-      isTradeSuccess ||
-      isTrade ||
-      isTradeIn ||
-      isTradeOut
-    ) {
+    if (match) {
       setTwaBg(true);
     } else {
       setTwaBg(false);
     }
-  }, [
-    isTrade,
-    isTradeIn,
-    isTradeOut,
-    isTradeSuccess,
-    isWithdrawSuccess,
-    location,
-  ]);
+  }, [match]);
 
   return (
     <Context.Provider value={{ navigationType }}>
